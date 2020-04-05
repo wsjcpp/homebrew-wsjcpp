37cb4f398b35373d07e35cfd3ee76db0eda18c84
Evgenii Sopov <mrseakg@gmail.com>
2020-04-05 20:34:41 +0700

    Fixed 'brew audit --strict wsjcpp'

--- README.md
+++ README.md
@@ -8,3 +8,9 @@ Just execute `brew tap wsjcpp/wsjcpp` and `brew install wsjcpp`.
 wget https://github.com/wsjcpp/wsjcpp/archive/v0.1.1.tar.gz
 shasum -a 256 v0.1.1.tar.gz
 ```
+
+## Checks for Homebrew
+
+```
+$ brew audit --strict wsjcpp
+```
--- wsjcpp.rb
+++ wsjcpp.rb
@@ -1,30 +1,30 @@
 class Wsjcpp < Formula
-    desc "Yet one... C++ Package Manager"
-    homepage "https://github.com/wsjcpp/wsjcpp"
-    version "0.1.1"
-    url "https://github.com/wsjcpp/wsjcpp/archive/v0.1.1.tar.gz"
-    sha256 "cf484645f3d857201a4e3302abbfd32bb1a61534cef262a0882902df4d99e58c"
-    head "https://github.com/wsjcpp/wsjcpp.git", :branch => "master"
-  
-    depends_on "cmake"
-    depends_on "curl-openssl"
-    depends_on "pkg-config"
-    depends_on :xcode
-    
-    bottle do
-        cellar :any
-        rebuild 1
-    end
-    
-    def install
-        bin.mkpath
-        system "cmake", "-DCMAKE_INSTALL_PREFIX:PATH=#{prefix}" , "."
-        system "make"
-        system "cp", "./wsjcpp", "#{prefix}/bin/wsjcpp"
-        bin.install_symlink
-    end
+  desc "Yet one... C++ Package Manager"
+  homepage "https://github.com/wsjcpp/wsjcpp"
+  url "https://github.com/wsjcpp/wsjcpp/archive/v0.1.1.tar.gz"
+  sha256 "cf484645f3d857201a4e3302abbfd32bb1a61534cef262a0882902df4d99e58c"
+  head "https://github.com/wsjcpp/wsjcpp.git", :branch => "master"
 
-    test do
-        system bin/"wsjcpp", "version"
-    end
+  bottle do
+    cellar :any
+    rebuild 1
   end
+
+  depends_on "cmake"
+  depends_on "curl-openssl"
+  depends_on "pkg-config"
+  depends_on :xcode
+
+  def install
+    bin.mkpath
+    system "cmake", "-DCMAKE_INSTALL_PREFIX:PATH=#{prefix}", "."
+    system "make"
+    bin.install "wsjcpp"
+    # system "cp", "./wsjcpp", "#{bin}/wsjcpp"
+    bin.install_symlink
+  end
+
+  test do
+    system bin/"wsjcpp", "version"
+  end
+end
